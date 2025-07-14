537ddda1eae40ab6291aafe1a42c75c65563af08
nandinidas07 <nandinidas107@gmail.com>
2025-07-14 19:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const winningCombos = [
+  [1,2,3], [4,5,6], [7,8,9],
+  [1,4,7], [2,5,8], [3,6,9],
+  [1,5,9], [3,5,7]
+];
+
+function checkWinner() {
+  winningCombos.forEach(combo => {
+    let [a, b, c] = combo;
+    if (
+      board[a] !== "" &&
+      board[a] === board[b] &&
+      board[a] === board[c]
+    ) {
+      showWinner(currentPlayerName);
+      highlightCells([a,b,c]);
+    }
+  });
+}
